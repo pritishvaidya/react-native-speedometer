c041f1293d601ee39f4337739401ce46b944a033
pritishvaidya <pritishvaidya94@gmail.com>
2018-12-29 14:27:14 +0530

    Updating the example

--- example/App.js
+++ example/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 
-import Speedometer from 'react-native-speedometer';
+import RNSpeedometer from 'react-native-speedometer';
 
 export default class App extends React.Component {
   state = {
     value: 0,
   }
 
-  onChange = value => this.setState({value: Number(value)})
+  onChange = value => this.setState({ value: parseInt(value) })
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput autoFocus style={styles.textInput} onChangeText={this.onChange} />
-        <Speedometer value={this.state.value} />
+        <TextInput placeholder="Speedometer Value" style={styles.textInput} onChangeText={this.onChange} />
+        <RNSpeedometer value={this.state.value} />
       </View>
     );
   }
@@ -23,12 +23,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 100,
   },
   textInput: {
-    marginHorizontal: 20,
     borderBottomWidth: 0.3,
     borderBottomColor: 'black',
+    height: 25,
+    fontSize: 16,
+    marginVertical: 50,
+    marginHorizontal: 20,
   },
 });
--- src/utils/limit-value.js
+++ src/utils/limit-value.js
@@ -1,6 +1,10 @@
 /* eslint radix: ["error", "as-needed"] */
 function limitValue(value, minValue, maxValue) {
-  return Math.min(Math.max(parseInt(value), minValue), maxValue);
+  let currentValue = 0
+  if(!isNaN(value)) {
+    currentValue = parseInt(value)
+  }
+  return Math.min(Math.max(currentValue, minValue), maxValue);
 }
 
 export default limitValue;
