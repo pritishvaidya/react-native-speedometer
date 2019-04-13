c13b263944bc60bc21c01de3086107ee20a0f67b
Grant Rygh <grantrygh@gmail.com>
2019-04-13 13:52:30 -0500

    ...

--- src/utils/limit-value.js
+++ src/utils/limit-value.js
@@ -4,9 +4,7 @@ function limitValue(value, minValue, maxValue, allowedDecimals) {
   let currentValue = 0;
   if (!isNaN(value)) {
     if (!isNaN(allowedDecimals) && allowedDecimals > 0) {
-      currentValue = parseFloat(value).toFixed(
-        allowedDecimals < 4 ? parseInt(allowedDecimals) : 4
-      );
+      currentValue = parseFloat(value).toFixed(allowedDecimals < 4 ? parseInt(allowedDecimals) : 4);
     } else {
       currentValue = parseInt(value);
     }
